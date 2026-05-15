f999fbf142351b36c57fec6d83cfd68c5878a99f
Gurveer56 <gs6901500@gmail.com>
2026-05-15 09:40:03 -0700

    feat(auth): add auth screens and navigation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,7 @@
 import { LoginScreen } from '@features/auth/screens/LoginScreen';
 
 export default function LoginRoute() {
+
   return <LoginScreen />;
+  
 }
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,5 +1,7 @@
 import { WelcomeScreen } from '@features/auth/screens/WelcomeScreen';
 
 export default function WelcomeRoute() {
+
   return <WelcomeScreen />;
+  
 }
